1cdf0b18f30e2a6a84e0020b4b4f54a5434972d3
Evelyn Atlas <>
2021-06-02 23:16:40 +0000

    Ugly Front Page Complete with Placeholders
    Have put all links, info, and placeholder images in the right places. The different sections e.g. footer are where they should be and are not doing anything strange like overlapping other content.

--- main.py
+++ main.py
@@ -45,5 +45,16 @@ def advocacy():
 def find_a_service():
   return render_template("find_service.html")
 
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+
+@app.route("/sign_up")
+def sign_up():
+  return render_template("sign_up.html")
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0', debug=True)
